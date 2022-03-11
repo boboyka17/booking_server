67897f1e87cbdd2b392de109d83201022d3a18c8
boboyka17 <boboyka15@gmail.com>
2022-03-11 14:22:13 +0700

    test

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
-const cors = require("cors");
-app.use(cors());
+// const cors = require("cors");
+// app.use(cors());
 
 // dp config
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
+// const mongoose = require("mongoose");
+// mongoose.connect(process.env.DATABASE_URL);
+// const db = mongoose.connection;
+// db.on("error", (error) => console.log(error));
 
-app.use(express.json());
+// app.use(express.json());
 
 // route setting
-const users = require("./routes/users");
-const events = require("./routes/events");
-const auth = require("./routes/auth");
+// const users = require("./routes/users");
+// const events = require("./routes/events");
+// const auth = require("./routes/auth");
 
-app.use("/api", users);
-app.use("/api", events);
-app.use("/api", auth);
+// app.use("/api", users);
+// app.use("/api", events);
+// app.use("/api", auth);
 
 app.listen(port, () => console.log("Server Started"));
