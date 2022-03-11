b8f479b6e8babff70685f35ba7ca78ee174ce457
boboyka17 <boboyka15@gmail.com>
2022-03-11 14:34:20 +0700

    fix heroku err

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-// require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
 const express = require("express");
 const app = express();
-const port = 3000;
-// const cors = require("cors");
-// app.use(cors());
+const cors = require("cors");
+app.use(cors());
 
 // dp config
-// const mongoose = require("mongoose");
-// mongoose.connect(process.env.DATABASE_URL);
-// const db = mongoose.connection;
-// db.on("error", (error) => console.log(error));
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
 
-// app.use(express.json());
+app.use(express.json());
 
 // route setting
-// const users = require("./routes/users");
-// const events = require("./routes/events");
-// const auth = require("./routes/auth");
+const users = require("./routes/users");
+const events = require("./routes/events");
+const auth = require("./routes/auth");
 
-// app.use("/api", users);
-// app.use("/api", events);
-// app.use("/api", auth);
+app.use("/api", users);
+app.use("/api", events);
+app.use("/api", auth);
 
-app.listen(port, () => console.log("Server Started"));
+app.listen(PORT, HOST, () => {
+  console.log("Server started on " + HOST + ":" + PORT);
+});
